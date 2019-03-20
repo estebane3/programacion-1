791485317a5f20ce5749d2abe4b6725e9a3d948d
estebane3 <47418174+estebane3@users.noreply.github.com>
2019-03-20 10:53:09 -0300

    Add files via upload

---
+++ Programacion-1/clase 1 de laboratorio/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main()
+{
+    int i; //declarar variable
+    i=0;     //le damos valor
+    while(i<10) // i es menor a 10
+    {
+        printf("%d\n",(i+1));
+        i++;
+    }
+
+    return 0;  //while = hacer(mientras)  fin hacer
+}
+*/
+
+
+
+/*int main ()
+{
+    int i;  //do_while = hacer meintras( condicion)
+    i=0;
+    do
+    {
+        i++;
+        printf("%d\n",i);
+    }
+    while(i<10);
+
+    return 0;
+}
+*/
+
+
+
+    int main() // aplicacion de FOR
+    {
+        int i;
+        for(i=0;i<10;i++)
+        {
+            printf("%d\n",(i+1));
+        }
+        return 0;
+    }
+
