558cadd9bfb7613d64c1cdf4cb0858f1d9a6fb55
esteban <esteban_2010_arias@hotmail.com>
2019-04-02 21:50:48 -0300

    esteban

--- tp (prueba)/main.c
+++ tp (prueba)/main.c
@@ -23,11 +23,11 @@ void menu(){
     int num2;
     int opciones;
 
-    printf("1. Sumar");
-    printf("\n2. Restar");
-    printf("\n3. Multiplicar");
-    printf("\n4. Dividir");
-    printf("\n5. Factorial");
+    printf("1. suma" );
+    printf("\n2. resta");
+    printf("\n3. multi");
+    printf("\n4. dividir");
+    printf("\n5. factorial");
 
     // ALMACENAMOS LA OPCION
     printf("\nDigite su opcion: ");
@@ -81,25 +81,53 @@ printf("El resultado de su calculo: %d " , num1 * num2);
 }
 void dividir(int num1, int num2) //LA DIVISION  RECIBE 2 PARAMETROS, LOS RESUELVE Y MUESTRA, NO HACE FALTA MANDARLO AL MAIN
 {
+    if (num2 == 0)
+    {
+        printf("Error. no se pudo dividir por 0\n");
+    }
+    else
+    {
+        printf("El resultado de su calculo: %d " , num1 / num2);
+    }
 
-printf("El resultado de su calculo: %d " , num1 / num2);
 
 }
 void factorial(int num1, int num2)  //EL FACTORIAL  RECIBE 2 PARAMETROS, LOS RESUELVE Y MUESTRA, NO HACE FALTA MANDARLO AL MAIN
 {
 int i;
+int i2;
 int factorial_1=1;
 int factorial_2=1;
 
-for(i=1;i<=num1;i++)
-{
+if (num1 > 0)
+    {
+        for(i=1;i<=num1;i++)
+    {
     factorial_1=factorial_1*i;
 
-}
-for(i=1;i<=num2;i++)
-{
-    factorial_2=factorial_2*i;
-}
-    printf("\nEl factorial del primer numero es: %d y el factorial del segundo numero es: %d",factorial_1, factorial_2);
-}
+    }
 
+    }
+    else
+    {
+        printf("El primer numero no tiene factorial \n");
+    }
+
+    if (num2 > 0)
+    {
+        for(i2=1;i2<=num2;i2++)
+    {
+    factorial_2=factorial_2*i2;
+    }
+
+    }
+    else
+    {
+        printf("El segundo numero no tiene factorial \n");
+    }
+
+    printf("El factorial de A es : %d y El factorial de B es: %d",factorial_1,factorial_2);
+
+
+
+}
